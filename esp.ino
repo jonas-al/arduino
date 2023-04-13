62220bc1ab11935bedf4a2675e55ad211e93c0d3
yonas <72419151+jonas-al@users.noreply.github.com>
2023-04-13 14:47:32 -0300

    Initial Commit

---
+++ esp.ino
@@ -0,0 +1,115 @@
+#if ! (ESP8266 || ESP32 )
+  #error This code is intended to run on the ESP8266/ESP32 platform! Please check your Tools->Board setting
+#endif
+
+#include "Credentials.h"
+
+#define MYSQL_DEBUG_PORT      Serial
+
+// Debug Level from 0 to 4
+#define _MYSQL_LOGLEVEL_      1
+
+#include <MySQL_Generic.h>
+
+#define USING_HOST_NAME     false
+
+#if USING_HOST_NAME
+  // Optional using hostname, and Ethernet built-in DNS lookup
+  char server[] = "topicos"; // change to your server's hostname/URL
+#else
+  IPAddress server(10, 16, 1, 20);
+#endif
+
+uint16_t server_port = 3306;    //3306;
+
+char default_database[] = "topicos";           //"test_arduino";
+char default_table[]    = "dadosTU";          //"test_arduino";
+
+String default_value    = "Hello, Arduino!"; 
+
+float temperatura = random(50);
+int umidade = random(100);
+int idSensor = 99;
+String usuario = String("qenga");
+
+// Sample query
+String INSERT_SQL = String("INSERT INTO ") + default_database + "." + default_table 
+                 + " (usuario, idSensor, tempCelsius, umidade) VALUES (usuario, idSensor, temperatura, umidade)";
+
+MySQL_Connection conn((Client *)&client);
+
+MySQL_Query *query_mem;
+
+void setup()
+{
+  Serial.begin(115200);
+  while (!Serial);
+
+  MYSQL_DISPLAY1("\nStarting Basic_Insert_ESP on", ARDUINO_BOARD);
+  MYSQL_DISPLAY(MYSQL_MARIADB_GENERIC_VERSION);
+
+  // Begin WiFi section
+  MYSQL_DISPLAY1("Connecting to", ssid);
+  
+  WiFi.begin(ssid, pass);
+  
+  while (WiFi.status() != WL_CONNECTED) 
+  {
+    delay(500);
+    MYSQL_DISPLAY0(".");
+  }
+
+  // print out info about the connection:
+  MYSQL_DISPLAY1("Connected to network. My IP address is:", WiFi.localIP());
+
+  MYSQL_DISPLAY3("Connecting to SQL Server @", server, ", Port =", server_port);
+  MYSQL_DISPLAY5("User =", user, ", PW =", password, ", DB =", default_database);
+}
+
+void runInsert(String query)
+{
+  // Initiate the query class instance
+  MySQL_Query query_mem = MySQL_Query(&conn);
+
+  if (conn.connected())
+  {
+    MYSQL_DISPLAY(INSERT_SQL);
+    
+    // Execute the query
+    // KH, check if valid before fetching
+    if ( !query_mem.execute(INSERT_SQL.c_str()) )
+    {
+      MYSQL_DISPLAY("Insert error");
+    }
+    else
+    {
+      MYSQL_DISPLAY("Data Inserted.");
+    }
+  }
+  else
+  {
+    MYSQL_DISPLAY("Disconnected from Server. Can't insert.");
+  }
+}
+
+void loop()
+{
+  MYSQL_DISPLAY("Connecting...");
+  
+  //if (conn.connect(server, server_port, user, password))
+  if (conn.connectNonBlocking(server, server_port, user, password) != RESULT_FAIL)
+  {
+    delay(500);
+    runInsert(INSERT_SQL);
+    conn.close();                     // close the connection
+  } 
+  else 
+  {
+    MYSQL_DISPLAY("\nConnect failed. Trying again on next iteration.");
+  }
+
+  MYSQL_DISPLAY("\nSleeping...");
+  MYSQL_DISPLAY("================================================");
+ 
+  delay(60000);
+}
